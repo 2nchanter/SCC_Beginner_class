a650531cacc7053e72bec0af679e8fb942c14d49
“2nchanter” <2nchanter@gmail.com>
2021-08-27 17:12:37 +0900

    end is start

--- final/app.py
+++ final/app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-# client = MongoClient('mongodb://test:test@localhost', 27017)
+# client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@localhost', 27017)
 db = client.dbhomework
 
 ## HTML 화면 보여주기
